2286909e7fbb8198b8ef23ba5060a2d707493352
goatennisedits <goatennisedits@gmail.com>
2025-06-12 17:07:37 -0300

    Update calculadora.test.js

--- calculadora.test.js
+++ calculadora.test.js
@@ -3,3 +3,9 @@ const { soma } = require('./calculadora');
 test('soma de 1 e 2 deve ser 3', () => {
   expect(soma(1, 2)).toBe(3);
 });
+
+test('soma de múltiplos números', () => {
+  expect(soma(1, 2, 3)).toBe(6);
+  expect(soma(-1, 0, 1)).toBe(0);
+  expect(soma(10)).toBe(10);
+});
